41a7aca235fcec299e3d63a083eb207b14dafd3d
jdnl <jdnl@users.noreply.github.com>
2025-01-09 08:52:40 -0800

    Use variables for server port and add server IP output

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0e2c8caa4b6378d8c"
-  instance_type = "t2.micro"
+  ami                    = "ami-0e2c8caa4b6378d8c"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
@@ -14,7 +14,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   // Force instance replacement on `terraform apply`
@@ -27,9 +27,20 @@ resource "aws_security_group" "instance" {
 
   // Allow incoming traffic on 8080 from all IPs
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+variable "server_port" {
+  description = "Web server incoming HTTP port"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "Public IP address of the server"
 }
