a473c9010651ed796cbc4fbef92cf748a63665f2
jdnl <jdnl@users.noreply.github.com>
2025-01-09 08:23:41 -0800

    Deploy single web server

--- main.tf
+++ main.tf
@@ -3,10 +3,33 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-01816d07b1128cd2d"
+  ami           = "ami-0e2c8caa4b6378d8c"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
     Name = "terraform-example"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  // Force instance replacement on `terraform apply`
+  // since user data only runs on first boot
+  user_data_replace_on_change = true
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  // Allow incoming traffic on 8080 from all IPs
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
