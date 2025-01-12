bafd264d4286521b57ad5dc7a4b9d285b828c3cb
jdnl <jdnl@users.noreply.github.com>
2025-01-12 10:27:57 -0800

    Add remote state S3 bucket

---
+++ ch03/01-remote-state-in-s3/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "jdnl-terraform-state"
+
+  lifecycle {
+    // Terraform-specific lifecycle setting that prevents deletion during `terraform destroy`
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  // Prevent creation and updates of public ACLs for both objcets and bucket
+  block_public_acls = true
+  // Prevents creation and updates of bucket policies granting public access
+  block_public_policy = true
+  // Ignores public ACLs if they alredy exist or are somehow added in thte future
+  ignore_public_acls = true
+  // Don't allow bucket to become public
+  restrict_public_buckets = true
+}
